5f7d203932ea23ade02cf5d8179e3ad44de54945
ajgoward <ajgoward@gmail.com>
2020-10-09 11:00:42 +0000

    started to render products in the all products view and adding too database

--- products/views.py
+++ products/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def all_stock(request):
 
-    return render(request, 'products/products.html')
+    products = Product.objects.all
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
