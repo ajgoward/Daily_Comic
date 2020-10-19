f865d057d9634030ce6b51c1297684ff951b6879
ajgoward <ajgoward@gmail.com>
2020-10-19 11:40:30 +0000

    cleaned css code up and set the bag contexts

--- Daily_Comic/settings.py
+++ Daily_Comic/settings.py
@@ -78,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.basket_contents',
             ],
         },
     },
@@ -164,8 +165,4 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
-
-
-
-
+STANDARD_DELIVERY_PERCENTAGE = 10
