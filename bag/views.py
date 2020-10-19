7a5f6f12132736e3f37d1d3630885dc24f66d1ab
ajgoward <ajgoward@gmail.com>
2020-10-19 13:16:15 +0000

    started to render the products in the bag and wired up the add button on product page by setting the quantity at 0 and using an if statement

--- bag/views.py
+++ bag/views.py
@@ -1,27 +1,30 @@
-from django.shortcuts import (
-    render, redirect, reverse, HttpResponse, get_object_or_404)
+from django.shortcuts import render, redirect
 
-from products.models import Product
 # Create your views here.
 
 
 def see_the_bag(request):
-    """ a view to return index """
-    return render(request, "bag/thebag.html")
+    """ A view that renders the baasket contents page """
+
+    return render(request, 'bag/thebag.html')
 
 
 def add_to_basket(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    """ Add a quantity of the specified product to the shopping basket """
+    quantity = 0
 
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})
-
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if quantity == 0:
+        quantity = 1
     else:
-        bag[item_id] = quantity
+        quantity = int(request.POST.get('quantity'))
 
-    request.session['bag'] = bag
-    print(request.session['bag'])
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('bag', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    request.session['bag'] = basket
     return redirect(redirect_url)
