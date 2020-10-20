15eb29e51cac75084a19e4aa4839db1b25b0055b
ajgoward <ajgoward@gmail.com>
2020-10-20 14:23:09 +0000

    added the product size and made the input to add quantity styled with buttons and javascript to make them function

--- bag/views.py
+++ bag/views.py
@@ -12,19 +12,32 @@ def see_the_bag(request):
 def add_to_basket(request, item_id):
     """ Add a quantity of the specified product to the shopping basket """
     quantity = 0
-
-    if quantity == 0:
-        quantity = 1
-    else:
-        quantity = int(request.POST.get('quantity'))
-
     redirect_url = request.POST.get('redirect_url')
+
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     basket = request.session.get('bag', {})
 
-    if item_id in list(basket.keys()):
-        basket[item_id] += quantity
+    if size:
+        if item_id in list(basket.keys()):
+            if size in basket[item_id]['items_by_size'].keys():
+                basket[item_id]['items_by_size'][size] += quantity
+            else:
+                basket[item_id]['items_by_size'][size] = quantity
+        else:
+            basket[item_id] = {'items_by_size': {size: quantity}}
     else:
-        basket[item_id] = quantity
+
+        if quantity == 0:
+            quantity = 1
+        else:
+            quantity = int(request.POST.get('quantity'))
+
+        if item_id in list(basket.keys()):
+            basket[item_id] += quantity
+        else:
+            basket[item_id] = quantity
 
     request.session['bag'] = basket
     return redirect(redirect_url)
