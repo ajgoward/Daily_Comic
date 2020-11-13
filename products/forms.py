e0cc017aee27825d6bc7f5782ad398ea555dad33
ajgoward <ajgoward@gmail.com>
2020-11-13 15:51:39 +0000

    added stylying to review and itemdetail page also diabled selector for product in review

--- products/forms.py
+++ products/forms.py
@@ -9,3 +9,8 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Reviews
         fields = ('__all__')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'product-text'
