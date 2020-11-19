5dce478544acab4db50f495f963579b1410ecabb
ajgoward <ajgoward@gmail.com>
2020-11-19 09:47:11 +0000

    added payment form and logic using stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from .forms import OrderForm
+from django.conf import settings
 
 
 def checkout(request):
-    basket = request.session.get('bag', {})
-
-    if not basket:
-        messages.error(request, "There's nothing in your basket at the moment")
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
+
     form = OrderForm()
+    template = 'checkout/checkout.html'
     context = {
         'form': form,
-        'basket': basket
+       
     }
-    template = 'checkout/checkout.html'
+
     return render(request, template, context)
