a4e904c27f9b8ac18f06a55a2eb1703c3804f235
ajgoward <ajgoward@gmail.com>
2020-11-23 15:53:11 +0000

    added to subscription views to render the data and styled

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -3,8 +3,12 @@ from django.db import models
 
 class Subscriptions(models.Model):
     Name = models.CharField(max_length=255)
+    image_url = models.URLField(
+        max_length=1024, blank=True)
+    image = models.ImageField(blank=True)
     Description = models.CharField(max_length=255)
     Recurring = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     stripeSubscriptionId = models.CharField(max_length=255)
     stripePricekey = models.CharField(max_length=255)
 
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Subscriptions
 
 # Create your views here.
 
 
 def subscription(request):
     """ a view to return subscribe """
-    return render(request, "subscriptions/subscribe.html")
+    subscriptions = Subscriptions.objects.all()
+    template = "subscriptions/subscribe.html"
+
+    context = {
+        'subscrip': subscriptions,
+    }
+    return render(request, template, context)
