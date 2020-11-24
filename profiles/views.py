a2744b699b9741ecd03ef4702a6acc81c80c4f62
ajgoward <ajgoward@gmail.com>
2020-11-24 14:10:35 +0000

    updated allauth and got users displying in the profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Userprofiles
 
 
 def theProfile(request):
+    profile = get_object_or_404(Userprofiles, user=request.user)
     template = 'profiles/theprofile.html'
-
-    return render(request, template)
+    context = {
+        'profile': profile
+    }
+    return render(request, template, context)
