ed4c681a49113c3e86358cba6e3f0221791eb09f
ajgoward <ajgoward@gmail.com>
2020-11-30 09:25:53 +0000

    updated profile to save chekout info

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.theProfile, name='theprofile'),
+    path('order_history/<order_number>', views.order_history, name='order_history'),
+
     ]
