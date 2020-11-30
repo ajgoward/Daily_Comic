24da36832c57be21974ba2632f64eab0faef2967
ajgoward <ajgoward@gmail.com>
2020-11-30 16:53:45 +0000

    added secret keys

--- Daily_Comic/settings.py
+++ Daily_Comic/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'eag^z%ov8pj@*uw-ez&h(g42ti7af=rendh#vh4+s*elw_+-(y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
