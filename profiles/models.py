920a949e53be60a58923b72e4de3f6d03bc6196e
ajgoward <ajgoward@gmail.com>
2020-11-30 09:04:46 +0000

    updated profile page and added profile form

--- profiles/models.py
+++ profiles/models.py
@@ -6,19 +6,26 @@ from django.dispatch import receiver
 from django_countries.fields import CountryField
 
 
-class Userprofiles(models.Model):
+class  Userprofiles(models.Model):
     """
     A user profile model for maintaining default
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Country *', null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
-    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
-    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20,
+                                            null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80,
+                                               null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80,
+                                               null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40,
+                                            null=True, blank=True)
+    default_county = models.CharField(max_length=80,
+                                      null=True, blank=True)
+    default_postcode = models.CharField(max_length=20,
+                                        null=True, blank=True)
+    default_country = CountryField(blank_label='Country',
+                                   null=True, blank=True)
 
     def __str__(self):
         return self.user.username
@@ -29,7 +36,7 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     Create or update the user profile
     """
-    # if created:
-    Userprofiles.objects.create(user=instance)
+    if created:
+        Userprofiles.objects.create(user=instance)
     # Existing users: just save the profile
-    # instance.Userprofiles.save()
+    instance.userprofiles.save()
--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.contrib import messages
 from .models import Userprofiles
-
+from .forms import ProfileForm
 
 def theProfile(request):
     profile = get_object_or_404(Userprofiles, user=request.user)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, 'Successfully updated your details!')
+            
+    orders = profile.orders.all()
     template = 'profiles/theprofile.html'
+    form = ProfileForm(instance=profile)
     context = {
-        'profile': profile
+        'form': form,
+        'orders': orders,
+        'on_profile': True,
     }
     return render(request, template, context)
